056c683e3182119fde5a833f7292e47702a72ef9
Selhack01 <selhack.businnes@gmail.com>
2024-07-09 10:14:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const arttir= () => {
+    const sonuc = document.getElementById("sonuc");
+    sonuc.innerText = Number(sonuc.innerText)+1;
+  }
+
+  const azalt= () => {
+    const sonuc = document.getElementById("sonuc");
+    sonuc.innerText = Number(sonuc.innerText)-1;
+  }
+
+  const sifirla= () => {
+    const sonuc = document.getElementById("sonuc");
+    sonuc.innerText = 0 ;
+  }
+
+  return <div className="App">
+    <div className="title">Counter</div>
+    <div id="sonuc">0</div>
+    <div className="buttons">
+    <button onClick={arttir}>+</button>
+    <button onClick={sifirla}>0</button>
+    <button onClick={azalt}>-</button>
     </div>
-  );
+  </div>;
 }
 
 export default App;
